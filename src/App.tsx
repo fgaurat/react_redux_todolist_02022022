76914aa977326ab5869801dd652fb547938ced6a
Frédéric Gaurat <fgaurat@eolem.com>
2022-02-02 12:41:46 +0100

    add FinalForm

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 import TodoList from './features/todos/components/TodoList';
 import TodoForm from './features/todos/components/TodoForm';
+import TodoFinalForm from './features/todos/components/TodoFinalForm';
 
 function App() {
   return (
@@ -10,6 +11,8 @@ function App() {
 
       <h1>TodoForm</h1>
       <TodoForm></TodoForm>
+      <h1>TodoFinalForm</h1>
+      <TodoFinalForm></TodoFinalForm>
       <hr/>  
       <h1>TodoList Redux</h1>  
       <TodoList></TodoList>
