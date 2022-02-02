17fa6fbb2bf4c270059c40eae00eed5465d5b4f2
Frédéric Gaurat <fgaurat@eolem.com>
2022-02-02 14:09:58 +0100

    ok

--- src/features/todos/components/TodoFinalForm/TodoFinalForm.tsx
+++ src/features/todos/components/TodoFinalForm/TodoFinalForm.tsx
@@ -1,5 +1,5 @@
-import React, { FormEvent } from 'react';
-import { Form, Field } from 'react-final-form'
+import React, { FormEvent, Suspense } from 'react';
+import { Form, Field,useFormState } from 'react-final-form'
 import { useDispatch } from 'react-redux';
 import { Todo } from '../../../../core/Todo';
 import { saveTodoAsync } from '../../slices/todoSlice';
@@ -8,29 +8,39 @@ import { saveTodoAsync } from '../../slices/todoSlice';
 const TodoFinalForm = () => {
 
     const dispatch = useDispatch()
-    
 	
 	const onSubmit = (todo:Todo)=>{
         dispatch(saveTodoAsync(todo))
     }
 
+	const onValidate = (values:Todo)=>{
+		const errors:any = {}
+		if(!values.title){
+			errors.title = 'Required'
+		}
+		return errors
+	}
+
     return (
 		<Form
 			onSubmit={onSubmit}
+			validate = {onValidate},
 			initialValues={{title: '', completed: false}}
 			render={({ handleSubmit,submitting,pristine}) => (
-			<form onSubmit={handleSubmit} className='form-inline mt-3 mb-3'>
-				<label className='sr-only' htmlFor='title'>Name</label>
-				<Field name='title' component='input' className='form-control mb-2 mr-sm-2' placeholder='Add todo...'/>
-				
-				<label className='sr-only' htmlFor='completed'>Completed ?</label>
-				<Field name='completed' component='input' type="checkbox" />
-				<br/>
-				<button type='submit' className='btn btn-primary mb-2' disabled={pristine || submitting}>
-					Submit
-				</button>
-			</form>
-			)}/>
+
+					<form onSubmit={handleSubmit} className='form-inline mt-3 mb-3'>
+						<label className='sr-only' htmlFor='title'>Name</label>
+						<Field name='title' component='input' className='form-control mb-2 mr-sm-2' placeholder='Add todo...'/>
+						
+						<label className='sr-only' htmlFor='completed'>Completed ?</label>
+						<Field name='completed' component='input' type="checkbox" />
+						<br/>
+						<button type='submit' className='btn btn-primary mb-2' disabled={pristine || submitting}>
+							Submit
+						</button>
+					</form>
+			)
+		}/>
 	);
 };
 
